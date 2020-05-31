7736173a811a1c975354456514b1d3661022572c
yavuz <yavuz@farmazon.com.tr>
2020-05-31 03:05:43 +0300

    chapter 2

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamuraiApp.Data
+{
+    //in efcore we have tot explicitly tell to context which sqlprovider and connection string we are using
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+
+        //the first time efcore instantiate samuraicontext class it will trigger this method
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source =(localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
+        }
+    }
+}
